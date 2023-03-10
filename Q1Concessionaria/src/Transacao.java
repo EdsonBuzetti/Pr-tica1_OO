a11b86f3e4a29a5c4e5739083eb7c2e5b923b13b
EdsonBuzetti <126629330+EdsonBuzetti@users.noreply.github.com>
2023-03-10 10:29:58 -0300

    Add files via upload

---
+++ Q1Concessionaria/src/Transacao.java
@@ -0,0 +1,8 @@
+public class Transacao {
+    private double valorCompra;
+    private double valorVenda;
+    private int tipoPagamento;
+   private String dataCompra;
+   private String dataVenda;
+   private int tipoTransacao;
+
